cc272bb76b14212eb23d2d73d3bd1c8aad75eac4
PanasheNyamayaro <montellpanashe43@gmail.com>
2025-11-18 12:30:42 +0200

    Create google_sheet.py

---
+++ utils/google_sheet.py
@@ -0,0 +1,16 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import streamlit as st
+
+def get_sheet():
+    creds_info = st.secrets["gcp_service_account"]
+    creds = Credentials.from_service_account_info(
+        creds_info,
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+    client = gspread.authorize(creds)
+    return client.open("Velor_Trading_Journal").sheet1
+
+def append_row_to_sheet(row):
+    sheet = get_sheet()
+    sheet.append_row(row)
