06e6288329a29198114b0532321dcd79052657e3
PanasheNyamayaro <montellpanashe43@gmail.com>
2025-11-20 06:48:45 +0200

    Update google_sheet.py

--- utils/google_sheet.py
+++ utils/google_sheet.py
@@ -3,13 +3,13 @@ from google.oauth2.service_account import Credentials
 import streamlit as st
 
 def get_sheet():
-    creds_info = st.secrets["gcp_service_account"]
+    creds_info = st.secrets["[gcp_service_account]"]
     creds = Credentials.from_service_account_info(
         creds_info,
         scopes=["https://www.googleapis.com/auth/spreadsheets"]
     )
     client = gspread.authorize(creds)
-    return client.open("Velor_Trading_Journal").sheet1
+    return client.open("Velor_Tading_Journal").sheet1
 
 def append_row_to_sheet(row):
     sheet = get_sheet()
