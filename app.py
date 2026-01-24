07158f14629aa22289776923ec24062ce549f9c0
PanasheNyamayaro <montellpanashe43@gmail.com>
2026-01-24 10:15:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ risk_pct = st.number_input("Risk %", min_value=0.0, max_value=100.0, format="%.2
 if risk_pct > 5:
     st.warning(f"{risk_pct}% risk is too high.",icon = "⚠️")
 else:
-    break
+    'break'
 account_size = st.number_input("Account Size (USD)", min_value=0.0, format="%.2f")
 swap = st.number_input("Swap", format="%.2f")
 commision = st.number_input("Commision", format="%.2f")
