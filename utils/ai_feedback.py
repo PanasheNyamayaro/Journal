8a204666a6c6634158f612dda9300ecc093cf84f
PanasheNyamayaro <montellpanashe43@gmail.com>
2026-02-09 11:51:49 +0200

    Update ai_feedback.py

--- utils/ai_feedback.py
+++ utils/ai_feedback.py
@@ -3,10 +3,10 @@ from huggingface_hub import InferenceClient
 import streamlit as st
 
 # 1. Access secret
-HF_API_TOKEN = st.secrets["huggingface"]
+HF_API_TOKEn = st.secrets["huggingface"]["HF_API_TOKEN"]
 
 # 2. Setup Client (No URL needed!)
-client = InferenceClient(api_key=HF_API_TOKEN)
+client = InferenceClient(api_key=HF_API_TOKEn)
 
 def generate_ai_feedback(trade_data: dict) -> str:
     prompt = f"You are a professional forex trader analyse this trade and give feedback in less than 60 words: {trade_data}"
