8e7e6b17fec68490f5a87105fbe8a53073bd7a2b
PanasheNyamayaro <montellpanashe43@gmail.com>
2026-02-15 08:36:55 +0200

    Update 2_Review.py

--- pages/2_Review.py
+++ pages/2_Review.py
@@ -4,15 +4,16 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
-with st.sidebar:
-    st.markdown(
-        """
-        <div style='text-align:center; margin-bottom:20px;'>
-            <img src='assets/logo.png' style='width:90px; border-radius:6px;' />
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+st.html("""
+    <style>
+        img[alt="Logo"] {
+            height:6rem !important;
+        }
+    </style>
+""")
+
+
+st.logo("logo.png")
 
 
 st.set_page_config(page_title="Velor Review", page_icon="📝", layout="centered")
